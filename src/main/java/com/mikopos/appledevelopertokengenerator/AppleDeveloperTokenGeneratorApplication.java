58bb77f44be80e663d7a1a6978371eee7086408c
Marios <mikopos@gmail.com>
2022-08-31 17:28:17 +0300

    Initial commit

---
+++ src/main/java/com/mikopos/appledevelopertokengenerator/AppleDeveloperTokenGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.mikopos.appledevelopertokengenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleDeveloperTokenGeneratorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AppleDeveloperTokenGeneratorApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/mikopos/appledevelopertokengenerator/AppleDeveloperTokenGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikopos.appledevelopertokengenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleDeveloperTokenGeneratorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
